02f6d4c867e34454ab94190259116f927da80983
Lutz Linke <lutz.linke@gmail.com>
2020-07-01 04:14:31 +0200

    LEDs Vorne

--- main.ts
+++ main.ts
@@ -1,1 +1,8 @@
-
+let pinLEDsVorne = 0
+function LEDsVorneRGB (rot: number, gruen: number, blau: number) {
+    pinLEDsVorne = 67
+    PCA9685.reset(pinLEDsVorne)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED7, 100 - rot, pinLEDsVorne)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED6, 100 - gruen, pinLEDsVorne)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED5, 100 - blau, pinLEDsVorne)
+}
